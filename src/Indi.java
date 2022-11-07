534ac70ab29c54d97c6287ce3cc2de8637ea4d4f
dogmash69 <103027893+dogmash69@users.noreply.github.com>
2022-11-07 11:49:18 +0100

    ok

---
+++ src/Indi.java
@@ -0,0 +1,14 @@
+public class Indi {
+    public static void main(String[] args) {
+        String title = "Indiana Jones and the Last Crusade";
+        Boolean watchedIt = true;
+        int releasedIn = 1989;
+        float rate = (float) 8.2;
+
+        System.out.println(title);
+        System.out.println(watchedIt);
+        System.out.println(releasedIn);
+        System.out.println(rate);
+
+    }
+}
